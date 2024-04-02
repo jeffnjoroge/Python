d570bce2d28ea13da002a348a25c5f40ae35a4e1
Jeff <jeff22njoroge@gmail.com>
2024-04-02 21:24:58 +0300

    message

---
+++ forloop.py
@@ -0,0 +1,6 @@
+students = ["mary", "john", "james", "andrew"]
+for xx in students:
+
+    if xx == "james":
+       break
+    print(xx)
---
+++ whileloop.py
@@ -0,0 +1,6 @@
+number = 1
+while number < 50:
+    print(number)
+    number = number + 2
+
+
