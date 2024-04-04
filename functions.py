8018b40225c44e613d547648fe5aee38eeaa033b
Jeff <jeff22njoroge@gmail.com>
2024-04-04 20:53:27 +0300

    message

---
+++ functions.py
@@ -0,0 +1,34 @@
+
+
+def tamatisha():
+    print("used to stop processes")
+
+def anzisha():
+    print("used to start processes")
+
+# tamatisha()
+
+def mult(tt):
+    return tt * 2
+# # jj = mult(60)
+# print(jj)
+
+# assignment
+def square(mm):
+    return mm * mm
+# ss = square(6)
+# print(ss)
+
+
+
+def difference(x , m):
+    return x - m
+aa = difference(66 , 22)
+# print(aa)
+
+
+
+
+
+
+
---
+++ square.py
@@ -0,0 +1,16 @@
+# import functions
+# hh = functions.mult(2)
+# print(hh)
+from functions import square
+hh = square(55)
+print(hh)
+
+
+
+
+
+
+
+
+
+
